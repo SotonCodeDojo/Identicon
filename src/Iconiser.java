a9cd434eb9d468c38389f3c40fcb649de6d878d3
Jamie Scott <github@jami.org.uk>
2016-03-10 19:50:16 +0000

    added iconiser class

---
+++ src/Iconiser.java
@@ -0,0 +1,15 @@
+import java.awt.image.BufferedImage;
+
+public class Iconiser {
+    private String input;
+
+    public Iconiser(String input) {
+        this.input = input;
+    }
+
+    public BufferedImage getIcon() {
+
+        return null;
+    }
+
+}
