b86284b8b9b3ef351d77d75c419a61ba5f8fe205
Jamie Scott <github@jami.org.uk>
2016-03-10 20:27:05 +0000

    made it what Huw wants

--- src/Iconiser.java
+++ src/Iconiser.java
@@ -2,34 +2,29 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Iconiser {
-    private String input;
 
-    public Iconiser(String input) {
-        this.input = input;
-    }
-
-    public BufferedImage getIcon(int width, int height) {
+    public BufferedImage getIcon(String input, int width, int height) {
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics2D g = image.createGraphics();
-        for (int i : getInputAsCodes()) {
+        for (int i : getInputAsCodes(input)) {
             addCharCodeToImage(i, g);
         }
         return image;
     }
 
-    private int[] getInputAsCodes() {
+    private int[] getInputAsCodes(String input) {
         char[] inputCharArray = input.trim().toCharArray();
         int[] charCodes = new int[inputCharArray.length];
         int i = 0;
         for (char c : inputCharArray) {
-            charCodes[i] = (int) c;
+            charCodes[i] = ((int) c) - 33;
             i++;
         }
         return charCodes;
     }
 
     private void addCharCodeToImage(int i, Graphics2D g) {
-
+        g.drawPolygon();
     }
 
 }
