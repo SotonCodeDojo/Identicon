daedebb13b2ae01ba03f2718824f1932c39cbf52
Jamie Scott <github@jami.org.uk>
2016-03-10 20:41:57 +0000

    i undid it a

--- src/Iconiser.java
+++ src/Iconiser.java
@@ -1,30 +1,7 @@
-import java.awt.*;
-import java.awt.image.BufferedImage;
+
 
 public class Iconiser {
 
-    public BufferedImage getIcon(String input, int width, int height) {
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = image.createGraphics();
-        for (int i : getInputAsCodes(input)) {
-            addCharCodeToImage(i, g);
-        }
-        return image;
-    }
 
-    private int[] getInputAsCodes(String input) {
-        char[] inputCharArray = input.trim().toCharArray();
-        int[] charCodes = new int[inputCharArray.length];
-        int i = 0;
-        for (char c : inputCharArray) {
-            charCodes[i] = ((int) c) - 33;
-            i++;
-        }
-        return charCodes;
-    }
-
-    private void addCharCodeToImage(int i, Graphics2D g) {
-        g.drawPolygon();
-    }
 
 }
